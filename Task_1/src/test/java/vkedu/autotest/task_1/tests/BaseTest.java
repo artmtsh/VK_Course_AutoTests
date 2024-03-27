7d646190d1122ce8aa903e88fe25fbe4a915c066
artmtsh <58192947+artmtsh@users.noreply.github.com>
2024-03-27 23:45:17 +0300

    Add homework1

---
+++ Task_1/src/test/java/vkedu/autotest/task_1/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package vkedu.autotest.task_1.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import vkedu.autotest.task_1.pages.LoginPage;
+
+public class BaseTest {
+  private static String baseURL = "https://ok.ru/";
+  public static WebDriver webDriver = new ChromeDriver();
+  public static LoginPage loginPage = new LoginPage();
+  @BeforeAll
+  public static void setUp() {
+    Configuration.baseUrl = baseURL;
+  }
+  @BeforeEach
+  public void setUpEach(){
+    loginPage = new LoginPage();
+    loginPage.open();
+  }
+  @AfterEach
+  public void after (){
+    Selenide.closeWindow();
+  }
+}
